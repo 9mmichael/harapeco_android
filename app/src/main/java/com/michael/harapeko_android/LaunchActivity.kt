c90608c492a7a6b68d7ce6e3bdccda24c030557d
9mmichael <ueno.michael@gmail.com>
2019-04-20 17:58:54 +0900

    Add CodeStyle SquareAndroid

--- app/src/main/java/com/michael/harapeko_android/LaunchActivity.kt
+++ app/src/main/java/com/michael/harapeko_android/LaunchActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 
 class LaunchActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.launch_activity)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.launch_activity)
+  }
 }
