9729a9efc6d4d550fc84400029155481a8a6f6b0
9mmichael <ueno.michael@gmail.com>
2019-04-20 17:36:35 +0900

    initial commit

---
+++ app/src/main/java/com/michael/harapeko_android/LaunchActivity.kt
@@ -0,0 +1,12 @@
+package com.michael.harapeko_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.launch_activity)
+    }
+}
